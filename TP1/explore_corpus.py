ac5aeee625fbc588bbba3912c32b88d8e08a685d
Marc HOUNTO <marchounto@MacBook-Air-de-Marc.local>
2019-09-19 19:14:07 -0400

    explore corpus fait manque la fonction get most qui fait chier un peu

--- TP1/explore_corpus.py
+++ TP1/explore_corpus.py
@@ -9,6 +9,7 @@ Dans toutes les fonctions de ce fichier, le paramètre `corpus` désigne une lis
 ]
 """
 import preprocess_corpus as pre
+import itertools
 
 
 def count_tokens(corpus):
@@ -34,14 +35,36 @@ def count_types(corpus):
                 words.append(word)
     return len(words)
 
+def get_occurence(item):
+    #get the occurence value in the tuple
+    return item[1]
 
 def get_most_frequent(corpus, n):
+    pair_word_count = [] 
+    most_frequent_pair_word_count = []
+    for sentence in corpus:
+        pairs = []
+        for element in sentence:
+            pairs.append((element,sentence.count(element)))
+        print(pairs)
+        pairs =[(sum(i[1] for i in group), key)for key, group in itertools.groupby(sorted(pairs, key = lambda i: i[0]), lambda i: i[0])]
+        pair_word_count.append(pairs)
+    d = {x:0 for x, _ in pair_word_count}
+    for word, count in pair_word_count: d[word] += count
+    pair_word_count = list(map(tuple, d.items()))
+    sorted(pair_word_count, key=get_occurence, reverse=True)
+    index =0
+    while index < n :
+        most_frequent_pair_word_count.append(pair_word_count[index])
+        index = index + 1
+    for pair in most_frequent_pair_word_count:
+        pair[1] = pair[1]/count_tokens(corpus)
+    return most_frequent_pair_word_count
     """
     Renvoie les n mots les plus fréquents dans le corpus, ainsi que leurs fréquences
 
     :return: list(tuple(str, float)), une liste de paires (mot, fréquence)
     """
-    pass
 
 
 def get_token_type_ratio(corpus):
@@ -49,7 +72,6 @@ def get_token_type_ratio(corpus):
     Renvoie le ratio nombre de tokens sur nombre de types
     """
     return count_tokens(corpus)/count_types(corpus)
-
 
 
 def count_lemmas(corpus):
@@ -67,8 +89,7 @@ def count_stems(corpus):
     """
     stemmed_corpus = pre.stem(corpus)
     stem_count = count_types(stemmed_corpus)
-    pass
-
+    
 
 def explore(corpus):
     """
@@ -84,8 +105,12 @@ def explore(corpus):
 
     (Les chiffres ci-dessus sont indicatifs et ne correspondent pas aux résultats attendus)
     """
-    print
-    pass
+    print("Nombre de tokens: " + str(count_tokens(corpus)) + '\n')
+    print("Nombre de types: " + str(count_types(corpus))+ '\n')
+    print("Les 15 mots les plus frequents du vocabulaire: " + str(get_most_frequent(corpus,15)) + '\n')
+    print("Le ratio est de :" + str(get_token_type_ratio(corpus)) + '\n')
+    print("Le nombre de stems est de :" + str(count_stems(corpus)) + '\n')
+    print("Le nombre de lemmes est de :" + str(count_lemmas(corpus)) + '\n')
 
 
 if __name__ == "__main__":
@@ -104,4 +129,10 @@ if __name__ == "__main__":
     Nombre de types: 709
     ...
     """
-    pass
+    filenames = ["./data/shakespeare_test", "./data/shakespeare_train"]
+    for filename in filenames : 
+        with open(filename + ".txt", "r") as f:
+            corpus = f.read()
+        segmented_corpus = pre.segmentize(corpus)
+        tokenized_corpus = pre.tokenize(segmented_corpus)
+        explore(tokenized_corpus)
